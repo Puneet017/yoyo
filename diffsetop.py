653230e7b9ad3fa71c67f61229ccb21283150926
Puneet017 <95585582+Puneet017@users.noreply.github.com>
2021-12-07 14:03:25 +0530

    created diff. set operation code
    this code display different set operation on the screen.

---
+++ diffsetop.py
@@ -0,0 +1,18 @@
+# Program to perform different set operations
+
+# sets variables are define
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+#different set operations  
+# union
+print("Union of E and N is :", E | N)
+  
+# intersection
+print("Intersection of E and N is :", E & N)
+  
+# difference
+print("Difference of E and N is :", E - N)
+  
+# symmetric difference
+print("Symmetric difference of E and N is :", E ^ N)
